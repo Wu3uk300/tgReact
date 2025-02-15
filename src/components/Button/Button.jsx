04007f2c51d36fbca9815d240ef2958361d8f719
Maksym Kondratov <geset300@gmail.com>
2025-02-15 16:52:28 +0100

    ?

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import "../Button/Button.css";
+import "./Button.css";
 const Button = (props) => {
-  return <button className={`button` + props.className} {...props} />;
+  return <button {...props} className={`button ` + props.className} />;
 };
 
 export default Button;
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import "./Form.css";
+import useTelegram from "../../hooks/useTelegram";
+const Form = () => {
+  const [country, setCountry] = useState("");
+  const [street, setStreet] = useState("");
+  const [selectOpt, setSelectOpt] = useState("ph");
+  const { tg } = useTelegram();
+
+  useEffect(() => {
+    tg.MainButton.setParams({
+      text: "Отправить данные",
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!street || !country) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+    }
+  }, [country, street]);
+
+  const onChangeCountry = (e) => {
+    setCountry(e.target.value);
+  };
+  const onChangeStreet = (e) => {
+    setStreet(e.target.value);
+  };
+  const onChangeOption = (e) => {
+    setSelectOpt(e.target.value);
+  };
+
+  return (
+    <div className="form">
+      <h3>Введите ваши данные</h3>
+      <input
+        onChange={onChangeCountry}
+        className="input"
+        type="text"
+        placeholder="Страна"
+        value={country}
+      />
+      <input
+        onChange={onChangeStreet}
+        className="input"
+        type="text"
+        placeholder="Улица"
+        value={street}
+      />
+      <select value={selectOpt} onChange={onChangeOption} className="select">
+        <option value="ph">Физ.Лицо</option>
+        <option value="jr">Юр.Лицо</option>
+      </select>
+    </div>
+  );
+};
+
+export default Form;
